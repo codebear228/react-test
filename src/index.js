6f482d940b0d91cbd653921456628d0cbf9337c2
IcebearDL <u201616984@hust.edu.cn>
2019-08-06 22:24:06 +0800

    Toggle practice

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Clock from './components/App/App';
+import Toggle from  './components/Toggle/Toggle'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Clock />,
     document.getElementById('root')
+);
+
+ReactDOM.render(
+    <Toggle />,
+    document.getElementById('root2')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
