2c569a0a4b42f43bc7ffea7dc06e5e1197fea9df
IcebearDL <u201616984@hust.edu.cn>
2019-09-14 15:53:14 +0800

    react test

---
+++ build/precache-manifest.177d6a417278fe3fd13478ccc3d58cc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b7559cf233f2da9d0cad3384fa374b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ecf948977ec2a67cd226",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9e1933cdb90427513a91",
+    "url": "./static/js/2.b9a3faab.chunk.js"
+  },
+  {
+    "revision": "ecf948977ec2a67cd226",
+    "url": "./static/js/main.53d9caf5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
